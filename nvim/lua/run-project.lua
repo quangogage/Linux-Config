0a471fedda88fb6165d0380ee8ef436bd5aef736
Callgage <gageprod@gmail.com>
2024-10-21 13:46:13 -0700

    Fixed issue with project launching logic.

--- nvim/lua/run-project.lua
+++ nvim/lua/run-project.lua
@@ -1,15 +1,11 @@
 
 function vim.g.run_project()
     local macunix = vim.fn.has('macunix')
-
-    if vim.fn.filereadable("main.lua") == 1 then
-        -- Assume if we find a main.lua that it is a Love2D project.
-        vim.cmd('!love .')
-    elseif vim.fn.filereadable('package.json') == 1 then
+    if vim.fn.filereadable('package.json') == 1 then
         vim.cmd('!npm run start')
-    elseif not macunix and vim.fn.filereadable('run.bat') == 1 then
+    elseif macunix==0 and vim.fn.filereadable('run.bat') == 1 then
         vim.cmd('!run.bat')
-    elseif macunix and vim.fn.filereadable('run.sh') == 1 then
+    elseif macunix==1 and vim.fn.filereadable('run.sh') == 1 then
         vim.cmd('!./run.sh')
     else
         vim.cmd('echo "Could not determine project type."')
