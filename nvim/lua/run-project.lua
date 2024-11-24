7e3ebc0e40fcf8e5a1c260437d84c27293c8871b
quangogage <gageprod@gmail.com>
2024-11-24 14:13:42 -0800

    Now checking for unix rather than macunix in my run script.

--- nvim/lua/run-project.lua
+++ nvim/lua/run-project.lua
@@ -1,15 +1,17 @@
-
 function vim.g.run_project()
-    local macunix = vim.fn.has('macunix')
+    local is_windows = vim.fn.has('win32') == 1
+    local is_unix = vim.fn.has('unix') == 1
+
     if vim.fn.filereadable('package.json') == 1 then
         vim.cmd('!npm run start')
-    elseif macunix==0 and vim.fn.filereadable('run.bat') == 1 then
+    elseif is_windows and vim.fn.filereadable('run.bat') == 1 then
         vim.cmd('!run.bat')
-    elseif macunix==1 and vim.fn.filereadable('run.sh') == 1 then
+    elseif is_unix and vim.fn.filereadable('run.sh') == 1 then
         vim.cmd('!./run.sh')
     else
         vim.cmd('echo "Could not determine project type."')
     end
 end
 
-vim.keymap.set("", "<F6>", vim.g.run_project , { noremap = true, silent = true })
+vim.keymap.set("", "<F6>", vim.g.run_project, { noremap = true, silent = true })
+
